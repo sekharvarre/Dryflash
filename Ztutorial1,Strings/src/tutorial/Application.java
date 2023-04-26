7f1f7ef0f13706cbcf46a23649e43440234cf2ac
sekhar <sekhar814@gmail.com>
2023-04-26 12:42:38 +0530

    commmit all files

---
+++ Ztutorial1,Strings/src/tutorial/Application.java
@@ -0,0 +1,23 @@
+package tutorial;
+
+public class Application {
+	public static void main(String args[]) {
+		int myInt= 5;
+		String name= "pavanisekhar";
+		
+		String True = "bondforever ";
+		
+		String Fulldata= name + True;
+		System.out.println(Fulldata);
+		
+		
+		System.out.println("this is:"+ name);
+		System.out.println("the integer is: "+myInt);
+		double myDouble=23.22;
+		System.out.println("my nymber is: "+ myDouble);
+		
+		
+		
+		
+	}
+}
