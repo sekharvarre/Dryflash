a36fd12089db7610c7566f4d52422c60fed61066
sekhar <sekhar814@gmail.com>
2023-04-26 12:23:42 +0530

    flash commit uploaded all files

---
+++ project/src/com/orchard/program/Employee.java
@@ -0,0 +1,43 @@
+package com.orchard.program;
+
+public class Employee {
+	 
+    public Employee(int i, String string, String string2, int j) {
+		// TODO Auto-generated constructor stub
+	}
+	public long getSalary() {
+		return salary;
+	}
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	// member variables
+    private int id;
+    private String name;
+    private String department;
+    private long salary;
+ 
+    // 4 arg parameterized constructor
+ 
+    // getters & setters
+ 
+    // toString()
+}
